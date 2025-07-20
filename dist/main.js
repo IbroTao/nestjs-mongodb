8ccbcdecaec136c2959a89d4ee7b35cc0cd79374
Taofeek <ibrahimtaofeek005@gmail.com>
2025-07-20 10:32:21 +0100

    create data type object for services

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-async function bootstrap() {
+async function runApp() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    await app.listen(8000);
 }
-bootstrap();
+runApp();
 //# sourceMappingURL=main.js.map
