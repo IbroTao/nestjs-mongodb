69e92b77293206698c1f7c8f0d1131788b183e1c
Taofeek <ibrahimtaofeek005@gmail.com>
2025-07-20 10:28:46 +0100

    create data type object for services

--- dist/users/users.controller.d.ts
+++ dist/users/users.controller.d.ts
@@ -1,2 +1,7 @@
+import { UserService } from "./users.service";
+import { CreateUserDto } from "./dto/users.dto";
 export declare class UserController {
+    private userService;
+    constructor(userService: UserService);
+    createUser(createUserDto: CreateUserDto): void;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,15 @@
 /* eslint-disable prettier/prettier*/
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { UserService } from "./users.service";
+import { CreateUserDto } from "./dto/users.dto";
 
 
 @Controller('users')
 export class UserController {
-    
+    constructor(private userService: UserService) {}
+
+    @Post('post')
+    createUser(@Body() createUserDto: CreateUserDto) {
+        console.log(createUserDto);
+    }
 }
