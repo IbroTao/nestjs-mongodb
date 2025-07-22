90830bff769fa186994a1e08c6086dd28c34f045
Taofeek <ibrahimtaofeek005@gmail.com>
2025-07-22 13:47:31 +0100

    changed module name

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-async function runApp() {
+async function testApp() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     await app.listen(8000);
 }
-runApp();
+testApp();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function runApp() {
+async function testApp() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8000);
 }
-runApp();
+testApp();
