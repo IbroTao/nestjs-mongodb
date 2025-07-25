b73080cb91332a4ffca2a09969db7f0aeef9bf4e
Taofeek <ibrahimtaofeek005@gmail.com>
2025-07-25 19:20:23 +0100

    added post controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './users/users.module';
+import { PostModule } from './posts/posts.module';
 
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nestjs-course'), UserModule],
+    MongooseModule.forRoot('mongodb://localhost:27017/nestjs-course'), UserModule, PostModule],
   controllers: [],
   providers: [],
 })
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -4,8 +4,14 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Post } from "src/schemas/Post.schema";
+import { CreatePostDto } from "./dto/CreatePost.dto";
 
 @Injectable()
 export class PostService {
     constructor(@InjectModel(Post.name) private postModel: Model<Post>) {}
+
+    createPost(createPostDto: CreatePostDto) {
+        const newPost = new this.postModel(createPostDto);
+        return newPost.save();
+    }
 }
