554ae48a2d26e7ffe3be61bc522a56466a691def
Taofeek <ibrahimtaofeek005@gmail.com>
2025-07-25 19:26:04 +0100

    added post controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,14 @@
 /* eslint-disable prettier/prettier*/
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreatePostDto } from "./dto/CreatePost.dto";
+import { PostService } from "./posts.service";
 
 @Controller('posts')
 export class PostController {
-    
+    constructor(private postService: PostService) {}
+
+    @Post('create')
+    createPost(@Body() createPostDto: CreatePostDto) {
+        return this.postService.createPost(createPostDto);
+    }
 }
